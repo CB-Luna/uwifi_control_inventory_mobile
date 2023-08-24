747720f7c65978c5846332a565d692318487229b
Uzziel Palma <uzzielpalma99@gmail.com>
2023-08-23 18:50:29 -0600

    Se limpian más archivos que no se necesitan

--- lib/screens/widgets/pdf/utils.dart
+++
@@ -1,6 +0,0 @@
-import 'package:intl/intl.dart';
-
-class Utils {
-  static formatDate(DateTime date) => DateFormat.yMd().format(date);
-  static formatDateHour(DateTime date) => DateFormat.yMd().add_Hms().format(date);
-}
